b06fdbcde0868a08b1f84e4fecd0f5afada8756d
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-09-29 23:55:04 +0200

    adding section Hero

--- src/components/coffepage/SectionHero.tsx
+++ src/components/coffepage/SectionHero.tsx
@@ -5,7 +5,7 @@ const SectionHero = () => {
     return (
 
         <section className='flex justify-center pt-24 pb-12 px-10 gap-2 '>
-            <div className="grid grid-cols-1 lg:grid-cols-2  justify-center  items-center gap-6">
+            <div className="grid grid-cols-1 lg:grid-cols-2  justify-center  items-center gap-6 border-2 border-black">
                 <div className="flex flex-col items-start gap-4 w-[588px] ">
                     <h3 className="font-semibold text-[18px] leading-6 text-[#2a5b45]">De la planta a tu taza</h3>
                     <h1 className='font-semibold text-[38px] leading-10 text-[#000000]'>El mejor café del mundo, ahora en tu casa.</h1>
@@ -15,7 +15,8 @@ const SectionHero = () => {
                         que puedas viajar desde la comodidad de tu
                         hogar.
                     </p>
-                    <div className="flex gap-4">
+
+                    <div className="flex gap-4 border-2 border-black md:ml-20">
                         <p className='bg-[#1f1815] text-[#fff] py-3 px-6 rounded'>
                             Descubrir orígenes
                         </p>
@@ -26,7 +27,7 @@ const SectionHero = () => {
                         </Link>
                     </div>
                 </div>
-                <div className="flex items-stretch w-[500px] pl-10 lg:pl-0 lg:w-[588px] rounded-[20px] bg-contain bg-no-repeat">
+                <div className="flex items-stretch border-2 border-black text-center  md:w-[90%] pl-10 lg:pl-0 lg:w-[588px] rounded-[20px] bg-contain bg-no-repeat">
                     <img src={coffeePlant} alt="coffeePlant" />
                 </div>
             </div>
