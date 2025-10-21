e3ffe9c117abe000236493257426f471d431aab7
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-10-21 22:04:56 +0200

    adding Footer

--- src/components/coffepage/Footer.tsx
+++ src/components/coffepage/Footer.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
 import { Link } from 'react-router-dom';
-import Coffe from "../assets/icon/coffee.png";
-import Phone from "../assets/icon/Phone.png";
-import Email from "../assets/icon/email2.png";
-import ButtonContact from './ButtonContact';
-import NavLink from './NavLink';
-import Logo from './Logo';
-import Copyright from './Copyright';
+import Coffe from "../../assets/icon/coffee.png";
+import Phone from "../../assets/icon/Phone.png";
+import Email from "../../assets/icon/email2.png";
+import Logo from '../Logo';
+//import ButtonContact from './ButtonContact';
+//import NavLink from './NavLink';
+//import Logo from './Logo';
+//import Copyright from './Copyright';
 
 const Footer = () => {
     return (
@@ -18,7 +18,7 @@ const Footer = () => {
                         <div className="flex flex-col items-start gap-4">
                             <h3 className='font-semibold text-lg leading-6'>Te ayudamos en</h3>
                             <div className="flex flex-col gap-4">
-                                <Link>
+                                <Link to="#">
                                     <ButtonContact
                                         className={'w-[182px] flex  bg-[#515051]  rounded-[4px] py-3 px-6 font-semibold text-sm leading-4 cursor-pointer ease-in duration-300 hover:bg-[#616363] items-center '}
                                         classContainer={'w-[134px] flex items-center gap-2'}
@@ -26,7 +26,7 @@ const Footer = () => {
                                         paragraph={'+34 919 49 05 18'}
                                     />
                                 </Link>
-                                <Link>
+                                <Link to="#">
                                     <ButtonContact
                                         className={'w-[232px] flex gap-2 bg-[#515051]  rounded-[4px] py-3 px-6 font-semibold text-sm leading-4 cursor-pointer ease-in duration-300 hover:bg-[#616363] items-center'}
                                         classContainer={'flex items-center'}
